04cadce9b5407708c744161aaaa4f48390f87ced
mmsshararii <mmssharariipc@gmail.com>
2026-03-12 19:55:38 +0300

    Update page.tsx

--- app/i/[id]/page.tsx
+++ app/i/[id]/page.tsx
@@ -1,18 +1,3 @@
-import { redirect } from 'next/navigation'
-import { decodeId } from '@/lib/short-id'
-import { supabase } from '@/lib/supabase'
-
-export default async function Page({ params }: { params: { id: string } }) {
-
-  const number = decodeId(params.id)
-
-  const { data } = await supabase
-    .from('posts')
-    .select('id')
-    .eq('short_id', number)
-    .single()
-
-  if (!data) redirect('/')
-
-  redirect(`/post/${data.id}`)
+export default function Page({ params }: { params: { id: string } }) {
+  return <div>short link: {params.id}</div>
 }
