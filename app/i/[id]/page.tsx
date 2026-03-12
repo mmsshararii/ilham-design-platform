963aedcbae937c7774fd289f47c8b1df542a3209
mmsshararii <mmssharariipc@gmail.com>
2026-03-12 19:46:23 +0300

    Update page.tsx

--- app/i/[id]/page.tsx
+++ app/i/[id]/page.tsx
@@ -1,1 +1,26 @@
 
+'use client'
+
+import { useParams, redirect } from 'next/navigation'
+import { decodeId } from '@/lib/short-id'
+import { supabase } from '@/lib/supabase'
+
+export default async function ShortLinkPage() {
+
+  const params = useParams()
+  const code = params.id as string
+
+  const number = decodeId(code)
+
+  const { data } = await supabase
+    .from('posts')
+    .select('id')
+    .eq('short_id', number)
+    .single()
+
+  if (!data) {
+    redirect('/')
+  }
+
+  redirect(`/post/${data.id}`)
+}
