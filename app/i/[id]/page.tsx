07769f6b30342482d576c77e75113f83c3766853
mmsshararii <mmssharariipc@gmail.com>
2026-03-13 16:04:29 +0300

    update short link behavior

--- app/i/[id]/page.tsx
+++ app/i/[id]/page.tsx
@@ -1,24 +1,43 @@
-import { redirect } from "next/navigation";
-import { supabase } from "@/lib/supabase";
-import { decodeId } from "@/lib/short-id";
+'use client';
 
-export default async function Page({ params }: any) {
+import { decodeId } from '@/lib/short-id';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { supabase, Post } from '@/lib/supabase';
+import PostDetailPage from '@/app/post/[id]/page';
 
-  const code = params.id;
+export default function ShortPostPage() {
 
-  const shortId = decodeId(code);
+  const params = useParams();
+  const code = params.id as string;
 
-  const { data } = await supabase
-    .from("posts")
-    .select("id")
-    .eq("short_id", shortId)
-    .limit(1);
+  const [post, setPost] = useState<Post | null>(null);
 
-  const post = data?.[0];
+  useEffect(() => {
+
+    const loadPost = async () => {
+
+      const shortId = decodeId(code);
+
+      const { data } = await supabase
+        .from('posts')
+        .select('*')
+        .eq('short_id', shortId)
+        .maybeSingle();
+
+      if (data) {
+        setPost(data);
+      }
+
+    };
+
+    loadPost();
+
+  }, [code]);
 
   if (!post) {
-    return <div style={{ padding: 40 }}>رابط غير صالح</div>;
+    return <div style={{ padding: 40 }}>جاري التحميل...</div>;
   }
 
-  redirect(`/post/${post.id}`);
+  return <PostDetailPage postId={post.id} />;
 }
