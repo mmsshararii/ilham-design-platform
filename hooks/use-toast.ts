d877ab99d3d3af62f31e3a14efff941c192ab85f
mmsshararii <mmssharariipc@gmail.com>
2026-03-19 05:37:50 +0300

    fix toast action type

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -1,22 +1,27 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, ReactNode } from 'react';
 
 type Toast = {
   id: string;
   title?: string;
   description?: string;
+  action?: ReactNode;
 };
 
 export function useToast() {
   const [toasts, setToasts] = useState<Toast[]>([]);
 
-  function toast({ title, description }: Omit<Toast, 'id'>) {
+  function toast({
+    title,
+    description,
+    action,
+  }: Omit<Toast, 'id'>) {
     const id = Math.random().toString();
 
     setToasts((prev) => [
       ...prev,
-      { id, title, description },
+      { id, title, description, action },
     ]);
 
     setTimeout(() => {
