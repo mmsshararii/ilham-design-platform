1f8bb1c2dd159c14487663edca816fc78c800f60
mmsshararii <mmssharariipc@gmail.com>
2026-03-19 07:23:07 +0300

    fix short page render

--- features/home/components/home-categories.tsx
+++ features/home/components/home-categories.tsx
@@ -37,13 +37,19 @@ export function HomeCategories() {
 
   {/* 👇 الخط السفلي */}
   <span
-    className={cn(
-      "absolute bottom-0 left-0 right-0 h-[3px] rounded-full transition-all duration-300",
+className={cn(
+  "relative px-4 pt-3 pb-4 text-sm font-medium whitespace-nowrap transition-all duration-200",
 
-      active === tab
-        ? "bg-purple-500 opacity-100"
-        : "opacity-0"
-    )}
+  // الوضع الطبيعي
+  "text-muted-foreground",
+
+  // 🔥 Hover (هذا المهم)
+  "hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-500 hover:to-blue-500",
+
+  // Active
+  active === tab &&
+    "text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500 font-semibold"
+)}
   />
 </button>
           );
