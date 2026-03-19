a194ad214c4e65d88aa335a7f8f1d366fcb43b36
mmsshararii <mmssharariipc@gmail.com>
2026-03-19 09:44:44 +0300

    fix short page render

--- features/home/components/home-feed.tsx
+++ features/home/components/home-feed.tsx
@@ -2,7 +2,7 @@
 
 export function HomeFeed() {
   return (
-    <div className="max-w-2xl mx-auto p-4 space-y-4">
+   <div className="w-full max-w-2xl p-4 space-y-4">
 
       <div className="border rounded-lg p-4">
         هذا منشور تجريبي
